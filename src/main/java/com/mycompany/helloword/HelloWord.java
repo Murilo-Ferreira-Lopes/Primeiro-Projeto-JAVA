2d2be8403388b61361c115e2c6d9bc2da9bb7f03
Murilo-ferreira-Lopes <murilo.f.lopes15@gmail.com>
2023-08-18 11:27:25 -0300

    Ola Mundo

---
+++ src/main/java/com/mycompany/helloword/HelloWord.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloword;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Murilo
+ */
+public class HelloWord {
+
+    public static void main(String[] args) {
+        JOptionPane.showInternalMessageDialog(null,"Olá Mundo!");
+    }
+}
